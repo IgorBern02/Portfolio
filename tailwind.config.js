775a81a19e0986b84dd073f2a22a1d94d0bc1577
IgorBern02 <igor.bernardes2@gmail.com>
2023-09-07 20:53:39 -0300

    Meu portfolio

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js}'],
+  darkMode: 'class',
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'purple': '#654AD7',
-      'mirage': '#1E192F',
-    },
+    extend: {
+      colors: {
+        purple: {
+          100: '#1E192F',
+          200: '#191970',
+          300: '#483D8B',
+          400: '#654AD7',
+        },
+        'black': '#000',
+      }
+    }, 
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
-}
-
+};
