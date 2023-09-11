5fb1f156f326cf7330fb50ef72a6421390c3a59e
IgorBern02 <igor.bernardes2@gmail.com>
2023-09-11 00:13:33 -0300

    .

--- src/script.js
+++ src/script.js
@@ -2,32 +2,7 @@ const html = document.querySelector("html");
 const chk = document.getElementById("chk");
 const ball = document.getElementById("ball");
 
-
-function toggleDarkMode(){
-    html.classList.toggle("dark");
-}
-
-// Save Theme
-function loadTheme() {
-    const darkMode = localStorage.getItem("dark");
-
-    if(darkMode){
-        toggleDarkMode();
-    }
-}
-
-loadTheme();
-
-
-// Add or Remove Dark Mode
 chk.addEventListener("change", () => {
-    toggleDarkMode();
+    html.classList.toggle("dark");
 
-    localStorage.removeItem("dark");
-
-    if(html.classList.contains("dark")){
-        localStorage.setItem("dark", 1);
-        
-    }
-
-})
+});
