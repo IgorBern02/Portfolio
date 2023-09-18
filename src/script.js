27505d22cc5b46880a0e50c51f0396d75d905264
Igor Bernardes <139928254+IgorBern02@users.noreply.github.com>
2023-09-18 17:50:57 -0300

    .

--- src/script.js
+++ src/script.js
@@ -6,3 +6,24 @@ chk.addEventListener("change", () => {
     html.classList.toggle("dark");
 
 });
+
+var el = document.querySelector('#name');
+var text = 'Igor Bernardes'
+var interval = 100;
+
+function showtext(el, text, interval) {
+    var char = text.split('').reverse();
+
+    var typer = setInterval(function() {
+
+        if (!char.length) {
+            return clearInterval(typer)
+        }
+
+        var next = char.pop();
+
+        el.innerHTML += next;
+    }, interval);
+}
+
+showtext(el, text, interval);
